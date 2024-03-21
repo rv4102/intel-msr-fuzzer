9f893e5b6d464a54d4f8bb18b90cbf7d91c8f69b
Rushil Venkateswar <rushilv14@gmail.com>
2024-03-21 19:03:41 +0530

    measurement working

--- main.sh
+++ main.sh
@@ -1,44 +1,54 @@
 #!/bin/bash
 
-if [ $# -ne 2 ]; then
-    echo "Usage: $0 <asm_file.asm> <input.cpp>"
+if [ $# -ne 1 ]; then
+    echo "Usage: $0 <input.cpp>"
     exit 1
 fi
 
-asm_file="$1"
-input_file="$2"
+input_file="$1"
 output_file="${input_file%.cpp}_modified.cpp"
 
-# Loop over each instruction in the asm file
-num_instructions=$(wc -l < "$asm_file")
+# get line number of the asm volatile block
+asm_volatile_line=$(grep -n "asm volatile" "$input_file" | cut -d: -f1)
+
+# get number of lines between the opening bracket of asm volatile block and closing bracket
+num_instructions=$(grep -n "asm volatile" "$input_file" | cut -d: -f1 | while read line; do
+    sed -n "${line},/);/p" "$input_file" | wc -l
+done)
+num_instructions=$((num_instructions-5))
+
+# Copy the original file to a new file
+cat "$input_file" > "temp.cpp"
+
+mkdir -p outputs
+
 for ((i=1; i<=num_instructions; i++)); do
-    # Create asm files for each i
-    ./inst_split.sh "$asm_file" "$i"
+    j=$(($asm_volatile_line+$i))
+    echo "Line $j is being measured!"
 
-    if [ -s "${asm_file%.asm}_first_instructions.asm" ]; then
-        echo
-    else
-        echo "nop" > "${asm_file%.asm}_first_instructions.asm"
-    fi
-    # Read lines from the replacement file and enclose them in double quotes
-    while IFS= read -r line; do
-        replacement_lines+="\"$line;\"\n"
-    done < "${asm_file%.asm}_first_instructions.asm"
-
-    
-    # Replace "nop" with the lines from the replacement file in the input file
-    sed "0,/\"nop\"/s//${replacement_lines}/" "$input_file" > "$output_file"
-
-    sed -i "0,/nop/s//$(cat ${asm_file%.asm}_ith_instruction.asm)/" "$output_file"
+    # at each iteration, uncomment the jth line
+    sed -e "${j}s/\/\/ //" "$input_file" > "$output_file"
+    cat "$output_file" > "$input_file" 
 
     # Compile the modified C code
     g++ -o output "$output_file" -L./measure -I./measure -l:libmeasure.a
 
     # Run the code
-    sudo taskset -c 1 ./output > "${i}_output.txt"
+    sudo taskset -c 1 ./output > "outputs/${i}_output.txt"
 
-    # Clean up the asm files
-    rm "${asm_file%.asm}_first_instructions.asm" "${asm_file%.asm}_ith_instruction.asm" "$output_file"
+    if [ $i -ne 1 ]; then
+        # subtract the previous output from the current output
+        paste "outputs/$((i-1))_output.txt" "outputs/${i}_output.txt" | awk '{print $1 - $2}' > "outputs/inst_${i}.txt"
+    else
+        cat "outputs/${i}_output.txt" > "outputs/inst_${i}.txt"
+    fi
+
+    # Clean up
+    rm output "$output_file"
 done
 
-echo "Compilation completed"
+# Restore the original file
+cat "temp.cpp" > "$input_file"
+rm "temp.cpp"
+
+echo "Run completed"
--- temp.asm
+++
@@ -1,4 +0,0 @@
-movl 0x10, %eax
-movl 0x14, %ebx
-addl %ebx, %eax
-imull %ebx, %eax
